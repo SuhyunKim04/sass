1920297861e5571fc193e0eecab55c8bedfe62b7
Suhyun Kim <kim00431@algonquinlive.com>
2022-12-20 20:54:27 -0500

    20221220

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
 const p = document.querySelector('.box3');
-p.style.backgroundColor = 'olive'
+p.style.backgroundColor = 'olive';
+
+function sum(a,b) {
+    return a + b;
+}
+
+function double (a) {
+    return a * a;
+}
+console.log(sum(3,5)) // 8
+console.log(sum(1,2)) // 3;
+console.log(double(3)) // 9
+console.log(double(5)) // 25
